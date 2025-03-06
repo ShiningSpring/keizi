20665f28dbfde6aa829cc147fd6db3b6f1f42ec6
ShiningSpring <harutoto1101@gmail.com>
2025-03-06 10:16:54 +0900

    aaa

--- keizi/app.py
+++ keizi/app.py
@@ -78,4 +78,4 @@ def handle_new_message(data):
 
 # アプリ起動
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True)
